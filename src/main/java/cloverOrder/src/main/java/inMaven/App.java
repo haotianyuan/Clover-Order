d2da3c9eebbb9ee7acc5b31b284f12ad9da79695
haotian yuan <haotianyuan@haotians-MacBook-Pro.local>
2020-08-30 20:11:11 -0700

    createOrder/addLineItem

--- src/main/java/cloverOrder/src/main/java/inMaven/App.java
+++ src/main/java/cloverOrder/src/main/java/inMaven/App.java
@@ -18,14 +18,33 @@ public final class App {
     private final static String merchantId = "263E8B8MBKAE1";
     private final static String apiToken = "7cfea3cb-1f13-d262-8282-eb0445537c8e";
     private static int totalPrice = 10088;
+    private static String orderId = "";
 
     public static void main(String[] args) throws IOException {
+        String lineItemId = "T1QE73HYF7TZ2";
         System.out.println("=========Clover Order===========");
-        //createOrder();
-        getOrder();
+        //getMenu();
+        createOrder();
+        addLineItem(lineItemId);
     }
 
-    private static void createOrder() throws IOException {
+    private static void getMenu() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+            .url("https://sandbox.dev.clover.com/v3/merchants/263E8B8MBKAE1/items?access_token=7cfea3cb-1f13-d262-8282-eb0445537c8e")
+            .get()
+            .build();
+
+        try (Response response = client.newCall(request).execute()){
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void createOrder() throws IOException{
         OkHttpClient client = new OkHttpClient();
 
         MediaType mediaType = MediaType.parse("application/json");
@@ -35,20 +54,28 @@ public final class App {
         Request request = new Request.Builder()
                 .url("https://sandbox.dev.clover.com/v3/merchants/" + merchantId + "/orders?access_token=" + apiToken)
                 .post(body).addHeader("content-type", "application/json").build();
-
-        Response response = client.newCall(request).execute();
+        try (Response response = client.newCall(request).execute()){
+            orderId = response.body().string().split(",")[1].split(":")[1].substring(2,15);
+            //System.out.println(orderId);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static void getOrder() throws IOException {
+    private static void addLineItem(String lineItemId) throws IOException {
         OkHttpClient client = new OkHttpClient();
 
+        MediaType mediaType = MediaType.parse("application/json");
+        String requestBody = "{\"item\":{\"id\":\""+lineItemId+"\"}}";
+        RequestBody body = RequestBody.create(mediaType, requestBody);
         Request request = new Request.Builder()
-        .url("https://sandbox.dev.clover.com/v3/merchants/263E8B8MBKAE1/orders?access_token=7cfea3cb-1f13-d262-8282-eb0445537c8e")
-        .get()
-        .addHeader("accept", "application/json")
+        .url("https://sandbox.dev.clover.com/v3/merchants/"+merchantId+"/orders/"+orderId+"/line_items?access_token="+apiToken)
+        .post(body)
+        .addHeader("content-type", "application/json")
         .build();
 
         Response response = client.newCall(request).execute();
-        System.out.println(response.body().toString());
     }
+
+    
 }
